36ffeb64b3fd7367ffea19930e6e416ab9523121
Glynis MacMillan <glynis.macmillan@gmail.com>
2013-08-01 22:14:16 -0400

    Created repository

---
+++ FizzBuzz.R
@@ -0,0 +1,17 @@
+
+################
+### FIZZBUZZ ###
+################
+
+# This program prints the numbers 1 to 100, but replaces numbers divisible by. 3 with "fizz", 
+# numbers divisible by 5 with "buzz", and numbers divisible by 3 and 5 with "fizzbuzz".
+# Written for R (version 3.0.1, Mac OSX) by G. MacMillan on 7/16/2013.
+
+
+for (i in 1:100){
+  if (i%%3==0) if (i%%5==0) print ("fizzbuzz")     # if divisible by 3 and 5, print fizzbuzz
+  if (i%%3==0) if (i%%5!=0) print ("fizz")         # if divisible by 3, print fizz
+  if (i%%5==0) if (i%%3!=0) print ("buzz")         # if divisible by 5, pring buzz
+  if (i%%3!=0) if (i%%5!=0) print(i)               # if divisible by neither, print the number
+} # end for loop
+
