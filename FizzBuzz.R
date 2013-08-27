8a1052ba63721731aefe47fdcbc407604ff87898
Glynis MacMillan <glynis.macmillan@gmail.com>
2013-08-27 13:40:38 -0400

    Update FizzBuzz.R

--- FizzBuzz.R
+++ FizzBuzz.R
@@ -1,4 +1,4 @@
-# This program prints the numbers 1 to 100, but replaces numbers divisible by. 3 with "fizz", 
+# This program prints the numbers 1 to 100, but replaces numbers divisible by 3 with "fizz", 
 # numbers divisible by 5 with "buzz", and numbers divisible by 3 and 5 with "fizzbuzz".
 # Created by Glynis MacMillan on 7/16/2013.
 
@@ -8,5 +8,5 @@ for (i in 1:100){
   if (i%%3==0) if (i%%5!=0) print ("fizz")         # if divisible by 3, print fizz
   if (i%%5==0) if (i%%3!=0) print ("buzz")         # if divisible by 5, pring buzz
   if (i%%3!=0) if (i%%5!=0) print(i)               # if divisible by neither, print the number
-} # end for loop
+} # end for 
 
